c61002b0f8d79bacbbde2b41b6c4fa6c51829e88
jensnti <jensandreasson77@gmail.com>
2021-10-14 16:47:20 +0200

    💬 data files

---
+++ src/_data/notes.js
@@ -0,0 +1,17 @@
+const Cache = require('@11ty/eleventy-cache-assets');
+
+module.exports = async () => {
+    try {
+        let url = 'http://localhost:3000/notes';
+
+        /* This returns a promise */
+        return Cache(url, {
+            duration: '1d',
+            type: 'json'
+        });
+    } catch (e) {
+        return {
+            // my failure fallback data
+        };
+    }
+};
